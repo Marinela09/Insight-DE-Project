6c147a3defc05cd5d4c81dcab3a2be1c37180169
Marinela Popova <marinela.g.popova@gmail.com>
2019-03-18 00:50:09 -0400

    minimize repetition in Airflow DAG definition

--- airflow/schedule_batch.py
+++ airflow/schedule_batch.py
@@ -14,112 +14,70 @@ default_args = {
 dag = DAG(
     'BatchScheduler', default_args=default_args, schedule_interval=timedelta(days=30))
 
+bash_command_template = ''' cd; cd /home/ubuntu/project;
+		  spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar
+		   --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077
+	           --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar
+                   /home/ubuntu/project/config/'''
+
 t1 = BashOperator(
     task_id='Compute_Jan',
-    bash_command=('cd; cd /home/ubuntu/project;'
-		  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-		  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-	          ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketJanuary.config' ),
+    bash_command=bash_command_template + 's3bucketJanuary.config',
     dag=dag)
 
 t2 = BashOperator(
     task_id='Compute_Feb',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketFebruary.config' ),
+    bash_command=bash_command_template + 's3bucketFebruary.config',
     dag=dag)
 
 t3 = BashOperator(
     task_id='Compute_Mar',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketMarch.config' ),
+    bash_command=bash_command_template + 's3bucketMarch.config',
     dag=dag)
 
 t4 = BashOperator(
     task_id='Compute_Apr',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketApril.config' ),
+    bash_command=bash_command_template + 's3bucketApril.config',
     dag=dag)
 
 t5 = BashOperator(
     task_id='Compute_May',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketMay.config' ),
+    bash_command=bash_command_template + 's3bucketMay.config',
     dag=dag)
 
 t6 = BashOperator(
     task_id='Compute_Jun',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketJune.config' ),
+    bash_command=bash_command_template + 's3bucketJune.config',
     dag=dag)
 
 t7 = BashOperator(
     task_id='Compute_Jul',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketJuly.config' ),
+    bash_command=bash_command_template + 's3bucketJuly.config',
     dag=dag)
 
 t8 = BashOperator(
     task_id='Compute_Aug',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketAugust.config' ),
+    bash_command=bash_command_template + 's3bucketAugust.config',
     dag=dag)
 
 t9 = BashOperator(
     task_id='Compute_Sep',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketSeptember.config' ),
+    bash_command=bash_command_template + 's3bucketSeptember.config',
     dag=dag)
 
 t10 = BashOperator(
     task_id='Compute_Oct',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketOctober.config' ),
+    bash_command=bash_command_template + 's3bucketOctober.config',
     dag=dag)
 
 t11 = BashOperator(
     task_id='Compute_Nov',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketNovember.config' ),
+    bash_command=bash_command_template + 's3bucketNovember.config',
     dag=dag)
 
 t12 = BashOperator(
     task_id='Compute_Dec',
-    bash_command=('cd; cd /home/ubuntu/project;'
-                  'spark-submit --jars /home/ubuntu/project/postgresql-42.2.5.jar'
-                  ' --class rankingsmall --master spark://ec2-34-239-206-246.compute-1.amazonaws.com:7077'
-                  ' --executor-memory 6G /home/ubuntu/project/target/scala-2.11/rankingsmall_2.11-1.0.jar'
-                  ' /home/ubuntu/project/config/s3bucketDecember.config' ),
+    bash_command=bash_command_template + 's3bucketDecember.config',
     dag=dag)
 
 
